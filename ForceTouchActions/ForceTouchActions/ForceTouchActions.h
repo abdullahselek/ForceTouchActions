bb27e9fe7adb397aedfc9d146a73f39900ac540a
abdullahselek <abdullahselek@gmail.com>
2016-08-19 22:39:20 +0200

    - Initial commit

---
+++ ForceTouchActions/ForceTouchActions/ForceTouchActions.h
@@ -0,0 +1,19 @@
+//
+//  ForceTouchActions.h
+//  ForceTouchActions
+//
+//  Created by Abdullah Selek on 19/08/16.
+//  Copyright © 2016 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ForceTouchActions.
+FOUNDATION_EXPORT double ForceTouchActionsVersionNumber;
+
+//! Project version string for ForceTouchActions.
+FOUNDATION_EXPORT const unsigned char ForceTouchActionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ForceTouchActions/PublicHeader.h>
+
+
